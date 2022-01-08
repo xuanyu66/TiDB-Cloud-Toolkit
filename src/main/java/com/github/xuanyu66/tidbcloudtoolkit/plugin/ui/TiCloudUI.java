ff75a314dd9356096d22d66cc926c72df045f659
shiyuhang0 <52435083+shiyuhang0@users.noreply.github.com>
2022-01-08 08:45:04 -0600

    ADD:create cluster (#19)

--- src/main/java/com/github/xuanyu66/tidbcloudtoolkit/plugin/ui/TiCloudUI.java
+++ src/main/java/com/github/xuanyu66/tidbcloudtoolkit/plugin/ui/TiCloudUI.java
@@ -1,5 +1,6 @@
 package com.github.xuanyu66.tidbcloudtoolkit.plugin.ui;
 
+import com.github.xuanyu66.tidbcloudtoolkit.backend.entity.ClusterVO;
 import com.github.xuanyu66.tidbcloudtoolkit.backend.entity.ClusterWrapper;
 import com.github.xuanyu66.tidbcloudtoolkit.plugin.JTableUtils;
 import com.github.xuanyu66.tidbcloudtoolkit.plugin.module.ClusterTableModel;
@@ -9,45 +10,59 @@ import java.awt.event.ActionListener;
 import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class TiCloudUI {
 
-    private JPanel mainPanel;
-    private JTabbedPane TiCloudTabbedPane;
-    private JPanel onePanel;
-    private JPanel twoPanel;
-    private JPanel threePanel;
-    private JButton createAClusterButton;
-    private JTable table1;
-    private JButton refreshButton;
-    private JPanel refresh;
-    private JButton deleteClusterButton;
-    private JButton setTrafficFiltersButton;
-    private JButton generateConfigButton;
+  private JPanel mainPanel;
+  private JTabbedPane TiCloudTabbedPane;
+  private JPanel onePanel;
+  private JPanel twoPanel;
+  private JPanel threePanel;
+  private JButton createAClusterButton;
+  private JTable table1;
+  private JButton refreshButton;
+  private JPanel refresh;
+  private JButton deleteClusterButton;
+  private JButton setTrafficFiltersButton;
+  private JButton generateConfigButton;
+  private JTextField nameTextField;
+  private JPasswordField passwordField;
+  private JPanel create;
 
-    public TiCloudUI() {
-        createAClusterButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                DataService.getDataService().createCluster("");
-            }
-        });
+  public TiCloudUI() {
+    createAClusterButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        ClusterVO clusterVO = new ClusterVO();
+        clusterVO.setName(nameTextField.getText().trim());
+        clusterVO.setRoot_password(passwordField.getText().trim());
+        try {
+          DataService.getDataService().createCluster(clusterVO);
+          JOptionPane.showMessageDialog(null, "创建成功");
+        } catch (Exception ex) {
+          JOptionPane.showMessageDialog(null, ex.getMessage(), "创建失败",
+              JOptionPane.INFORMATION_MESSAGE);
+        }
+      }
+    });
 
+    refreshButton.addActionListener(e -> {
+      List<ClusterWrapper> clusterWrappers = DataService.getDataService().listClusters();
+      ClusterWrapper clusterWrapper = clusterWrappers.get(0);
+      ClusterTableModel model = new ClusterTableModel();
+      table1.setModel(model);
+      model.add(clusterWrapper.getCluster());
+      JTableUtils.setTableStyle(table1);
+    });
+  }
 
-        refreshButton.addActionListener(e -> {
-            List<ClusterWrapper> clusterWrappers = DataService.getDataService().listClusters();
-            ClusterWrapper clusterWrapper = clusterWrappers.get(0);
-            ClusterTableModel model = new ClusterTableModel();
-            table1.setModel(model);
-            model.add(clusterWrapper.getCluster());
-            JTableUtils.setTableStyle(table1);
-        });
-    }
-
-    public JComponent getPanel() {
-        return mainPanel;
-    }
+  public JComponent getPanel() {
+    return mainPanel;
+  }
 }
