bd97dfec7daa5ca282e44377927836e5a7cdf18e
shiyuhang <1136742008@qq.com>
2022-01-08 16:00:16 +0800

    ADD:finish Setting UI

--- src/main/java/com/github/xuanyu66/tidbcloudtoolkit/plugin/service/DataService.java
+++ src/main/java/com/github/xuanyu66/tidbcloudtoolkit/plugin/service/DataService.java
@@ -1,36 +1,67 @@
 package com.github.xuanyu66.tidbcloudtoolkit.plugin.service;
 
+import com.github.xuanyu66.tidbcloudtoolkit.backend.entity.ClusterVO;
 import com.github.xuanyu66.tidbcloudtoolkit.backend.entity.ClusterWrapper;
+import com.github.xuanyu66.tidbcloudtoolkit.backend.service.TiDBCloudToolkitService;
+import com.github.xuanyu66.tidbcloudtoolkit.backend.service.TiDBCloudToolkitServiceImpl;
+import com.github.xuanyu66.tidbcloudtoolkit.backend.util.GsonUtil;
 import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.Service;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-@State(name = "DataService",storages = @Storage("plugin.xml"))
+@Service
+@State(name = "DataService", storages = @Storage("plugin.xml"))
 public class DataService implements PersistentStateComponent<DataState> {
-    private DataState state = new DataState();
 
-    public static DataService getInstance() {
-        return ServiceManager.getService(DataService.class);
+  private DataState state = new DataState();
+
+  private static volatile DataService dataService = null;
+
+  public static DataService getDataService() {
+    if (dataService == null) {
+      synchronized (DataState.class) {
+        if (dataService == null) {
+          dataService = new DataService();
+        }
+      }
     }
+    return dataService;
+  }
 
+  public static DataService getInstance() {
+    return ServiceManager.getService(DataService.class);
+  }
 
-    @Override
-    public @Nullable DataState getState() {
-        return state;
-    }
+  private TiDBCloudToolkitService toolkitService;
 
-    @Override
-    public void loadState(@NotNull DataState state) {
-        this.state = state;
-    }
+  @Override
+  public @Nullable DataState getState() {
+    return state;
+  }
 
-    public List<ClusterWrapper> getGids(){
-        return state.getClusterLists();
-    }
+  @Override
+  public void loadState(@NotNull DataState state) {
+    this.state = state;
+  }
+
+  public List<ClusterWrapper> getGids() {
+    return state.getClusterLists();
+  }
+
+  public void login(String name, String password) {
+    toolkitService = new TiDBCloudToolkitServiceImpl(name, password);
+  }
+
+  public boolean createCluster(String cluster) {
+    return toolkitService.createCluster(GsonUtil.from(cluster, ClusterVO.class));
+  }
+
 
 }
