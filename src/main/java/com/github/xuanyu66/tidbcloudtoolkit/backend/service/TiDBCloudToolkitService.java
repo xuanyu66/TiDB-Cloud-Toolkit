90a155d2f19a3d5dc1749bf49c6cfd3770c20b8f
Leon Yang <1395305816@qq.com>
2022-01-08 12:25:02 +0800

    Revert "ADD: token interface (#11)"
    This reverts commit bae829c1946332a8ed7ca2bed8b7c1595a871674.

--- src/main/java/com/github/xuanyu66/tidbcloudtoolkit/backend/service/TiDBCloudToolkitService.java
+++ src/main/java/com/github/xuanyu66/tidbcloudtoolkit/backend/service/TiDBCloudToolkitService.java
@@ -15,8 +15,6 @@ public interface TiDBCloudToolkitService {
    */
   void refreshToken(String name, String password);
 
-  void refreshToken(String token);
-
   /**
    * get Developer Tier Cluster detail
    *
